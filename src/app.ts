9c892978f7a1208bb27afa0b2ea7bcf5f55de85f
rejacopyright <reja.copyright@gmail.com>
2024-11-21 17:57:32 +0700

    chore: Cors

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import cors, { CorsOptions } from 'cors'
 
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
@@ -21,6 +22,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(cors())
 
 app.use('/static', express.static(path.join(__dirname, '../public')))
 
